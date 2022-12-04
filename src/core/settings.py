1c76602bbcb5a873684fa661874c686d3706a9a4
Loverine Hall <obrianhenry10@gmail.com>
2022-12-03 18:08:42 -0700

    fix security

--- src/core/settings.py
+++ src/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-39@f-&1yea$309ry^$gup7#23hc9f3*pb#5jun!u-9u@h3)d29'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
